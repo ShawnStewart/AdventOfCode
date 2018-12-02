aa5a0808c0460187f83eee8081ba594dc768f22a
Shawn Stewart <s.stewart0220@gmail.com>
2018-12-02 18:43:40 -0500

    Completed Day 1 Part 1

--- 2018/Day01/index.js
+++ 2018/Day01/index.js
@@ -1,1 +1,10 @@
-console.log(process.argv[2]);
+const fs = require("fs");
+const argv = process.argv.slice(2);
+
+const data = fs.readFileSync(argv[0], "utf8").split(/[\r\n]+/g);
+
+const result = data.reduce((accum, cur) => {
+  return parseInt(accum) + parseInt(cur);
+});
+
+console.log(result);
